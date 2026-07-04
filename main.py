e8b22551aef6a10db8117e9c16dada0f049a3c95
DeSanta <anton2007df@gmail.com>
2026-07-04 14:24:52 +0500

    Add greetBlack function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def greetWhite():
     print("hello white")
 
+def greetBlack():
+    print("hello black")
+
 def main():
-    print("hello")
+    print("hello everyone")
 
 if __name__ == "__main__":
     main()
